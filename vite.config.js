c4fee1dfb74c269e40a435e651ae558da3eb4b87
SUNNY SHARMA <sharmasunny1684@gmail.com>
2025-09-07 10:23:51 +0530

    solve vite.congf

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src") // @ points to src folder
+      "@": path.resolve(__dirname, "src"), // @ points to src folder
     },
   },
-});
+  base: command === "build" ? "/Portfolio/" : "/", // dev ke liye "/", build ke liye repo name
+}));
